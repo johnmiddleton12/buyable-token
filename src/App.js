e3d1f712201a6539f44f2c699d4b06b4b3acef64
Johnathan <johnathanmiddleton1@gmail.com>
2022-06-21 23:19:03 -0700

    simple ethers integration with ropsten contract

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import { ethers } from "ethers";
+
+const CONTRACT_ADDRESS = '0xb5aC181F8C523B784e401ea31Bca89bfc5d4f283';
+
+const CONTRACT_ABI = [
+    "function buyToken() public virtual payable"
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+
+    const [address, setAddress] = useState('');
+    const [balance, setBalance] = useState(0);
+
+    async function connectToWallet() {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const accounts = await provider .send('eth_requestAccounts', []);
+        setAddress(accounts[0]);
+        const balance = await provider.getBalance(accounts[0]);
+        setBalance(ethers.utils.formatEther(balance));
+    };
+
+    async function makeTransaction() {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+
+        // call buyToken, sending 1 ether in the transaction
+        const tx = await contract.buyToken({
+            value: ethers.utils.parseEther('.01')
+        });
+        console.log(tx);
+
+    }
+
+    useEffect(() => {
+        connectToWallet();
+    }, []);
+
+    return (
+        <div className="App">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Your address: {address}
         </p>
+        <p>
+        Your balance: {balance}
+        </p>
+        <p>
+        Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={makeTransaction}>Make transaction</button>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
         >
-          Learn React
+        Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+        </div>
+    );
 }
 
 export default App;
